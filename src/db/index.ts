e9dcd11c449181f8bc85b9e7c519c2da0cef6bdf
Brian Cooper <brian@brian-cooper.com>
2026-01-18 13:30:42 -0800

    chore: wip

--- src/db/index.ts
+++ src/db/index.ts
@@ -52,7 +52,7 @@ export function getDb(): ReturnType<typeof drizzle<typeof schema>> {
  */
 export const db = new Proxy({} as ReturnType<typeof drizzle<typeof schema>>, {
   get(_target, prop) {
-    return (getDb() as Record<string | symbol, unknown>)[prop];
+    return (getDb() as unknown as Record<string | symbol, unknown>)[prop];
   },
 });
 
